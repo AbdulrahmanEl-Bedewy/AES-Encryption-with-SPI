63dce11c119e086316e313b126c225ce317a03d2
Abdulrahman Ayman <Abdulrahman.elbedewy02@eng-st.cu.edu.eg>
2023-04-17 17:14:23 +0200

    Update AES_Encrypt.v

--- AES/AES_Encrypt.v
+++ AES/AES_Encrypt.v
@@ -20,11 +20,63 @@ SPI_sub #(.DATA_OUT(128)) SPI_sub_inst(
 reg [127:0] Msg,Encrypted_Msg;
 
 wire [(128*(Nr+1))-1:0] AllKeys;
-wire [127:0] states [Nr+1:0];
+wire [7:0] state[0:3][0:3];
 wire [127:0] pSub,aSub;
 
 //key expansion
+KeyExpansion KeyEx(
+    .key(key) //[(Nk*32)-1:0] key,
+    .w(AllKeys) //reg [(128*(Nr+1))-1:0] w
+);
 
+
+always @(*) begin
+    
+    if(done ==1)begin
+
+        AddRoundKey k1(
+            .state(Msg)
+            .key(AllKeys[0+:128])
+            .ostate(Msg)
+        );
+        genvar j;
+        generate
+            for(j=1; j<Nr; j=j+1) begin
+                SubBytes subBytes_inst1(
+                    .istate(Msg),
+                    .ostate(Msg)
+                );
+                ShiftRows shiftRows_inst1(
+                    .istate(Msg),
+                    .ostate(Msg)
+                );
+                MixColumns mixColumns_inst1(
+                    .state(Msg),
+                    .ostate(Msg)
+                );
+                AddRoundKey k2(
+                    .state(Msg)
+                    .key(AllKeys[j*128+:128])
+                    .ostate(Msg)
+                );
+            end
+        endgenerate
+        SubBytes subBytes_inst2(
+            .istate(Msg),
+            .ostate(Msg)
+        );
+        ShiftRows shiftRows_inst2(
+            .istate(Msg),
+            .ostate(Msg)
+        );
+        AddRoundKey k3(
+            .state(Msg)
+            .key(AllKeys[Nr*128+:128])
+            .ostate(Msg)
+        );
+        Encrypted_Msg = Msg;
+    end
+end
 
 
 
