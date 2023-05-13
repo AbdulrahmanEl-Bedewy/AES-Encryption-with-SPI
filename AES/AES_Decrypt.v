97b8d8160505b2ae41ad7b785839948053c07de0
Abdulrahman Ayman <Abdulrahman.elbedewy02@eng-st.cu.edu.eg>
2023-05-13 23:29:36 +0300

    InvCipher wrapper and test bench

--- AES/AES_Decrypt.v
+++ AES/AES_Decrypt.v
@@ -1,1 +1,126 @@
-sa
+module AES_Dencrypt(
+    input cs,
+    input sclk,
+    input sdi,
+    output sdo
+);
+
+// SPI variable
+reg [0:127] tx;
+wire [0:257] rx;
+wire done;
+
+// Sizes and stage variable
+reg flag;
+reg [0:3] Nk;
+reg [0:3] Nr;
+
+// Key expansion variables
+// reg cs_Exp;
+reg [0:(8*32)-1] key;
+wire [0:(128*(14+1))-1] w;
+
+// InvCipher module msg and final output
+reg [0:127] init;
+wire [0:127] Decrypted_Msg128;
+wire [0:127] Decrypted_Msg196;
+wire [0:127] Decrypted_Msg256;
+
+SPI_Sub SPI1(
+    .cs(cs),
+    .sclk(sclk),
+    .sdi(sdi),
+    .tx(tx), // sent from AES module
+    .rx(rx), // sent to AES module
+    .sdo(sdo),
+    .done(done)
+);
+
+KeyExpansion KE1 (
+    .Nk(Nk),
+    .Nr(Nr),
+    .key(key),
+    .w(w)
+);
+
+InvCipher #(4,10) C1 (
+    .init(init),
+    .w(w[0:(128*(10+1))-1]),
+    .Decrypted_Msg(Decrypted_Msg128)
+);
+
+InvCipher #(6,12) C2 (
+    .init(init),
+    .w(w[0:(128*(12+1))-1]),
+    .Decrypted_Msg(Decrypted_Msg196)
+);
+
+InvCipher #(8,14) C3 (
+    .init(init),
+    .w(w[0:(128*(14+1))-1]),
+    .Decrypted_Msg(Decrypted_Msg256)
+);
+
+localparam Key = 0;
+localparam Message = 1;
+localparam Decryption = 2;
+reg [0:1] state;
+
+
+initial begin
+    flag = 0;
+    tx = 0;
+    state = 0;
+end
+
+
+always @(posedge done) begin
+	case(state) 
+		Key: begin
+			if(done==1)begin
+				case(rx[0:1])
+					 2'b00: begin 
+						  Nk = 4;
+						  Nr = 10;
+					 end
+					 2'b01: begin 
+						  Nk = 6;
+						  Nr = 12;
+					 end
+					 2'b10: begin 
+						  Nk = 8;
+						  Nr = 14;
+					 end
+				endcase
+				state = Message;
+				key = rx[2:257];
+			end
+		end
+		Message: begin
+			if(done == 1) begin
+				init = rx[257-:128];
+				state = Decryption;
+			end
+		end
+		Decryption: begin
+			state = Key;
+		end
+	endcase
+	
+end
+
+always @(posedge sclk)begin 
+	case(Nk)
+		 4'd4: begin 
+			  tx = Decrypted_Msg128;
+		 end
+		 4'd6: begin 
+			  tx = Decrypted_Msg196;
+		 end
+		 4'd8: begin 
+			  tx = Decrypted_Msg256;
+		 end
+	endcase
+end 
+
+endmodule
---
+++ Test Benches/AES_Decrypt_tb.v
@@ -0,0 +1,112 @@
+module AES_Decrypt_tb(
+    output [0:127] rxMain
+);
+
+
+
+reg [0:257] txMain;
+//wire [0:127] rxMain;
+
+wire miso;
+reg start;
+reg clk;
+wire cs_n;
+wire sclk;
+wire mosi;
+wire done;
+wire doneSub;
+
+
+SPI_Main spM(
+	.clk(clk),
+	.miso(miso),
+	.start(start),
+	.tx(txMain),
+	.rx(rxMain),
+	.cs_n(cs_n),
+	.sclk(sclk),
+	.mosi(mosi),
+	.done(done)
+);
+
+
+AES_Dencrypt Decrypt(
+    .cs(cs_n),
+    .sclk(sclk),
+    .sdi(mosi),
+    .sdo(miso)
+);
+
+initial begin
+	clk = 1'b0;
+	start = 1'b0;
+//=============================TEST 1============================================
+	txMain = 130'h000102030405060708090a0b0c0d0e0f;
+//	txMain = 130'h2b7e151628aed2a6abf7158809cf4f3c;
+	#20 start = 1'b1;
+	#20 start = 1'b0;
+	#1800
+	$display("Key Used = %h", txMain);
+	
+    txMain = 130'h69c4e0d86a7b0430d8cdb78070b4c55a;
+	#20 start = 1'b1;
+	#20 start = 1'b0;
+	#1800
+	$display("Msg Used = %h", txMain);
+	
+	txMain = 130'h0;
+	#20 start = 1'b1;
+	#20 start = 1'b0;
+	#1800
+	$display("Decrypted Msg = %h", rxMain);
+//=============================TEST 2============================================
+	txMain = 258'h000102030405060708090a0b0c0d0e0f1011121314151617;
+	txMain = {2'b01, txMain[2:257]};
+//	txMain = 130'h2b7e151628aed2a6abf7158809cf4f3c;
+	#20 start = 1'b1;
+	#20 start = 1'b0;
+	#2800
+	$display("Key Used = %h", txMain);
+	
+    txMain = 130'hdda97ca4864cdfe06eaf70a0ec0d7191;
+	#20 start = 1'b1;
+	#20 start = 1'b0;
+	#1800
+	$display("Msg Used = %h", txMain);
+	
+	txMain = 130'h0;
+	#20 start = 1'b1;
+	#20 start = 1'b0;
+	#1800
+	$display("Decrypted Msg = %h", rxMain);
+
+//=============================TEST 3============================================
+	txMain = 258'h000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f;
+	txMain = {2'b10, txMain[2:257]};
+//	txMain = 130'h2b7e151628aed2a6abf7158809cf4f3c;
+	#20 start = 1'b1;
+	#20 start = 1'b0;
+	#2800
+	$display("Key Used = %h", txMain);
+	
+    txMain = 130'h8ea2b7ca516745bfeafc49904b496089;
+	#20 start = 1'b1;
+	#20 start = 1'b0;
+	#1800
+	$display("Msg Used = %h", txMain);
+	
+	txMain = 130'h0;
+	#20 start = 1'b1;
+	#20 start = 1'b0;
+	#1800
+	$display("Decrypted Msg = %h", rxMain);
+	
+end
+
+
+
+always begin
+	#1 clk = ~clk;
+end
+
+endmodule
