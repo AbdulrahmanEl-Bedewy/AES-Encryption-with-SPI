152394fb21443b6fb2fb74ceaf5840bb4dc6f0aa
Abdulrahman Ayman <Abdulrahman.elbedewy02@eng-st.cu.edu.eg>
2023-05-16 19:05:46 +0300

    Decipher

--- AES/InvCipher.v
+++ AES/InvCipher.v
@@ -1,55 +1,91 @@
-module InvCipher #(parameter Nk=4,parameter Nr=10)(
-    input [0:127]init, 
-    input [0:128*(Nr+1)-1] w,
-    output [0:127] Decrypted_Msg
+module InvCipher ( //Nk = 4 for 128 bit key, 6 for 192 bit key, 8 for 256 bit key
+    input cs,
+    input clk,
+    input [0:3] Nr,
+    input [0:127]init,
+    input [0:(128*(14+1))-1] w,
+    output [0:127] Decrypted_Msg,
+    output reg flag
 );
 
-wire [0:127] state [0:Nr],aSub[0:Nr],aShift[0:Nr],aAdd[0:Nr];
+localparam InitialKey = 1;
+localparam SubByte = 2;
+localparam Shift = 3;
+localparam AddKey = 4;
+localparam Mix = 5;
+
+reg [0:127] iW, iKey , iSub, iShift, iMix, temp;
+wire [0:127] oKey , oSub, oShift, oMix;
+reg [0:3] state;
+reg [0:3] round;
 
 
 AddRoundKey k1(
-    .istate(init),
-    .key(w[Nr*128+:128]),
-    .ostate(state[Nr])
+    .istate(iKey),
+    .key(iW),
+    .ostate(oKey)
+);
+InvSubBytes subBytes_inst1(
+    .istate(iSub),
+    .ostate(oSub)
+);
+InvShiftRows shiftRows_inst1(
+    .istate(iShift),
+    .ostate(oShift)
+);
+InvMixColumns mixColumns_inst1(
+    .istate(iMix),
+    .ostate(oMix)
 );
 
-genvar round;
-generate
-    for(round=Nr-1; round>=1; round=round-1) begin :invcipher
-        InvShiftRows shiftRows_inst1(
-            .istate(state[round+1]),
-            .ostate(aShift[round])
-        );
-        InvSubBytes subBytes_inst1(
-            .istate(aShift[round]),
-            .ostate(aSub[round])
-        );
-        AddRoundKey k2(
-            .istate(aSub[round]),
-            .key(w[round*128+:128]),
-            .ostate(aAdd[round])
-        );
-        InvMixColumns mixColumns_inst1(
-            .istate(aAdd[round]),
-            .ostate(state[round])
-        );
+
+initial begin
+    round = 1;
+    state = InitialKey;
+    flag = 0;
+end
+
+always @(negedge clk) begin
+	if(cs == 0)begin
+			round = Nr-1;
+			state = InitialKey;
     end
-    InvShiftRows shiftRows_inst2(
-        .istate(state[1]),
-        .ostate(aShift[0])
-    );
-    InvSubBytes subBytes_inst2(
-        .istate(aShift[0]),
-        .ostate(aSub[0])
-    );
-    AddRoundKey k3(
-        .istate(aSub[0]),
-        .key(w[0+:128]),
-        .ostate(state[0])
-    );
-endgenerate
-assign Decrypted_Msg = state[0];
+    else if(round != 15)begin       
+        case(state)
+            InitialKey: begin
+                iW = w[Nr*128+:128];
+                iKey = init;
+                state = Shift;
+                flag = 0;
+            end
+            Shift: begin
+                state = SubByte;
+                if(round == Nr - 1)
+                    iShift = oKey;
+                else
+                    iShift = oMix;
+            end
+            SubByte: begin
+                iSub = oShift;
+                state = AddKey;
+            end
+            AddKey: begin
+                iW = w[128*round+:128];  
+                iKey = oSub;
+                round = round - 1;
+                state = Mix;
+            end
+            Mix: begin
+                iMix = oKey;
+                state = Shift;
+            end
+        endcase
+    end
+    else begin
+        flag = 1;
+    end
+end
 
-
+assign Decrypted_Msg = oKey;
 
 endmodule
--- Test Benches/InvCipher_tb.v
+++ Test Benches/InvCipher_tb.v
@@ -3,30 +3,52 @@ module InvCipher_tb(
 );
 
 
-localparam Nr = 10;
-localparam Nk = 4;
+reg [0:3] Nr;
+reg [0:3] Nk;
 
 reg [127:0] in;
-reg [32*Nk-1:0] key;
-wire [(128*(Nr+1))-1:0] w;
+reg [255:0] key;
+wire [1919:0] w;
 
-//KeyExpansion #(Nk,Nr)KE1 (
-//    .key(key),
-//    .w(w)
-//);
+KeyExpansion KE1 (
+    .Nk(Nk),
+    .Nr(Nr), 
+    .key(key),
+    .w(w)
+);
 
-InvCipher #(Nk,Nr) C1 (
-    .init(in),
-    .w(w),
-    .Decrypted_Msg(out)
+reg cs;
+reg clk;
+wire [0:127] Msg;
+wire flag;
+
+InvCipher C1 (
+	.cs(cs),
+	.clk(clk),
+	.Nr(Nr),
+	.init(in),
+	.w(w),
+	.Decrypted_Msg(out),
+	.flag(flag)
 );
 
 initial begin
-    in = 128'h69c4e0d86a7b0430d8cdb78070b4c55a;
+	 Nr = 10;
+	 Nk = 4;
+	 clk = 0;
+	 cs = 0;
+	 in = 128'h69c4e0d86a7b0430d8cdb78070b4c55a;
     key = 128'h000102030405060708090a0b0c0d0e0f;
+	 
+	 #10 cs = 1;
+	 
     
-    #100;
+    #1000;
     $display("out = %h \n",out);
+end
+
+always begin
+	#5 clk = ~clk;
 end
 
 
